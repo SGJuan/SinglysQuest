fe0c415e3a0f6ed79f25c6f50987de3afe05eca3
BuildTools <unconfigured@null.spigotmc.org>
2018-09-23 19:16:27 +0200

    added fov&fow systems

--- rl.py
+++ rl.py
@@ -1,28 +1,42 @@
 ﻿import libtcodpy as libtcod
 
+#Window Size
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 
+#Map Size
 MAP_WIDTH = 80
 MAP_HEIGHT = 45
 
-LIMIT_FPS = 20
-
+#Parameters for dungeon generator
 ROOM_MAX_SIZE = 10
 ROOM_MIN_SIZE = 6
 MAX_ROOMS = 30
 
+FOV_ALGO = 0  #default FOV algorithm
+FOV_LIGHT_WALLS = True
+TORCH_RADIUS = 10
+
+LIMIT_FPS = 20
+
+color_light_wall = libtcod.Color(130, 110, 50)
+color_light_ground = libtcod.Color(200, 180, 50)
 color_dark_wall = libtcod.Color(0, 0, 100)
 color_dark_ground = libtcod.Color(50, 50, 150)
 
+
 class Tile:
+    #a tile of the map and its properties
     def __init__(self, blocked, block_sight = None):
         self.blocked = blocked
+        self.explored = False
 
+        #by default, if a tile is blocked, it also blocks sight
         if block_sight is None: block_sight = blocked
         self.block_sight = block_sight
 
 class Rect:
+    #a rectangle on the map. used to characterize a room.
     def __init__(self, x, y, w, h):
         self.x1 = x
         self.y1 = y
@@ -30,15 +44,18 @@ class Rect:
         self.y2 = y + h
 
     def center(self):
-       	center_x = (self.x1 + self.x2) / 2
-       	center_y = (self.y1 + self.y2) / 2
-       	return (center_x, center_y)
+        center_x = (self.x1 + self.x2) / 2
+        center_y = (self.y1 + self.y2) / 2
+        return (center_x, center_y)
 
-    def intersect (self, other):
-       	return (self.x1 <= other.x2 and self.x2 >= other.x1 and 
-       			self.y1 <= other.y2 and self.y2 >= other.y1)
+    def intersect(self, other):
+        #returns true if this rectangle intersects with another one
+        return (self.x1 <= other.x2 and self.x2 >= other.x1 and 
+                self.y1 <= other.y2 and self.y2 >= other.y1)
 
 class Object:
+    #This is generic object: the player, a monster, an item, the stairs
+    #It's always represented by a character on screen.
     def __init__(self, x, y, char, color):
         self.x = x
         self.y = y
@@ -46,21 +63,26 @@ class Object:
         self.color = color
 
     def move(self, dx, dy):
+        #move
         if not map[self.x + dx][self.y + dy].blocked:
             self.x += dx
             self.y += dy
             print('You are x: ' + str(self.x) + ' and at y: ' + str(self.y))
 
     def draw(self):
-        libtcod.console_set_default_foreground(con, self.color)
-        libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
+        #draw
+        if libtcod.map_is_in_fov(fov_map, self.x, self.y):
+            libtcod.console_set_default_foreground(con, self.color)
+            libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
+
 
     def clear(self):
+        #clear
         libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
 
 def create_room(room):
     global map
-
+    #
     for x in range(room.x1 + 1, room.x2):
         for y in range(room.y1 + 1, room.y2):
             map[x][y].blocked = False
@@ -68,72 +90,98 @@ def create_room(room):
 
 def create_h_tunnel(x1, x2, y):
     global map
+    #
     for x in range(min(x1, x2), max(x1, x2) + 1):
         map[x][y].blocked = False
         map[x][y].block_sight = False
 
 def create_v_tunnel(y1, y2, x):
     global map
+    #
     for y in range(min(y1, y2), max(y1, y2) + 1):
         map[x][y].blocked = False
         map[x][y].block_sight = False
 
 def make_map():
     global map, player
+
+    #
     map = [[ Tile(True)
         for y in range(MAP_HEIGHT) ]
             for x in range(MAP_WIDTH) ]
 
     rooms = []
-    num_room = 0
+    num_rooms = 0
 
     for r in range(MAX_ROOMS):
-    	w = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
-    	h = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
-    	x = libtcod.random_get_int(0, 0, MAP_WIDTH - w - 1)
-    	y = libtcod.random_get_int(0, 0, MAP_HEIGHT - h - 1)
+        w = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        h = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        x = libtcod.random_get_int(0, 0, MAP_WIDTH - w - 1)
+        y = libtcod.random_get_int(0, 0, MAP_HEIGHT - h - 1)
 
-    	new_room = Rect(x, y, w, h)
+        new_room = Rect(x, y, w, h)
 
-    	failed = False
-    	for other_room in rooms:
-    		if new_room.intersect(other_room):
-    			failed = True
-    			break
+        failed = False
+        for other_room in rooms:
+            if new_room.intersect(other_room):
+                failed = True
+                print("failed to gen")
+                break
 
-    	if not failed:
-    		create_room(new_room)
-    		(new_x, new_y,) = new_room.center()
+        if not failed:
+            create_room(new_room)
+            (new_x, new_y) = new_room.center()
+            room_no = Object(new_x, new_y, chr(65+num_rooms), libtcod.white)
+            objects.insert(0, room_no) 
 
-    		if num_room == 0:
-    			player.x = new_x
-    			player.y = new_y
+            if num_rooms == 0:
+                player.x = new_x
+                player.y = new_y
+                #npc.x = new_x
+                #npc.y = new_y
 
-    		else:
-    			(prev_x, prev_y) = rooms[num_room-1].center()
+            else:
+                (prev_x, prev_y) = rooms[num_rooms-1].center()
 
-    			if libtcod.random_get_int(0, 0, 1) == 1:
-    				create_h_tunnel(prev_x, new_x, prev_y)
-    				create_v_tunnel(prev_y, new_y, new_x)
+                if libtcod.random_get_int(0, 0, 1) == 1:
+                    create_h_tunnel(prev_x, new_x, prev_y)
+                    create_v_tunnel(prev_y, new_y, new_x)
 
-    			else:
-    				create_v_tunnel(prev_y, new_y, prev_x)
-    				create_h_tunnel(prev_x, new_x, prev_y)
+                else:
+                    create_v_tunnel(prev_y, new_y, prev_x)
+                    create_h_tunnel(prev_x, new_x, new_y)
 
-    		rooms.append(new_room)
-    		num_room += 1
+            rooms.append(new_room)
+            num_rooms += 1
 
 def render_all():
     global color_light_wall, color_dark_wall
     global color_light_ground, color_dark_ground
+    global fov_recompute, fov_map
 
-    for y in range(MAP_HEIGHT):
-        for x in range(MAP_WIDTH):
-            wall = map[x][y].block_sight
-            if wall:
-                libtcod.console_set_char_background(con, x, y, color_dark_wall, libtcod.BKGND_SET)
-            else:
-                libtcod.console_set_char_background(con, x, y, color_dark_ground, libtcod.BKGND_SET)
+    if fov_recompute:
+        fov_recompute = False
+        libtcod.map_compute_fov(fov_map, player.x, player.y, TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGO)
+
+        for y in range(MAP_HEIGHT):
+            for x in range(MAP_WIDTH):
+                visible = libtcod.map_is_in_fov(fov_map, x, y)
+                wall = map[x][y].block_sight
+                if not visible:
+                    if map[x][y].explored:
+                        if wall:
+                            libtcod.console_set_char_background(con, x, y, color_dark_wall, libtcod.BKGND_SET)
+                        else:
+                            libtcod.console_set_char_background(con, x, y, color_dark_ground, libtcod.BKGND_SET)
+
+
+                else:
+
+                    if wall:
+                        libtcod.console_set_char_background(con, x, y, color_light_wall, libtcod.BKGND_SET)
+                    else:
+                        libtcod.console_set_char_background(con, x, y, color_light_ground, libtcod.BKGND_SET)
+                    map[x][y].explored = True
 
     for object in objects:
         object.draw()
@@ -141,7 +189,7 @@ def render_all():
     libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
 
 def handle_keys():
-    
+    global fov_recompute
     key = libtcod.console_wait_for_keypress(True)
     if key.vk == libtcod.KEY_ENTER and key.lalt:
         libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
@@ -151,15 +199,19 @@ def handle_keys():
 
     if libtcod.console_is_key_pressed(libtcod.KEY_UP):
         player.move(0, -1)
+        fov_recompute = True
 
     elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
         player.move(0, 1)
+        fov_recompute = True
 
     elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
         player.move(-1, 0)
+        fov_recompute = True
 
     elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
         player.move(1, 0)
+        fov_recompute = True
 
     key_char = chr(key.c)
 
@@ -179,11 +231,18 @@ libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, "Singly's Adventure", Fal
 #creating a now variable that is a console
 con = libtcod.console_new(SCREEN_WIDTH, SCREEN_WIDTH)
 player = Object(25, 23, '@', libtcod.white)
-npc = Object(25, 23, '!', libtcod.yellow)
+npc = Object(25, 23, 'N', libtcod.yellow)
 
 objects = [npc, player]
 
 make_map()
+
+fov_map = libtcod.map_new(MAP_WIDTH, MAP_HEIGHT)
+for y in range(MAP_HEIGHT):
+    for x in range(MAP_WIDTH):
+        libtcod.map_set_properties(fov_map, x, y, not map[x][y].block_sight, not map[x][y].blocked)
+
+fov_recompute = True
 
 while not libtcod.console_is_window_closed():
 
